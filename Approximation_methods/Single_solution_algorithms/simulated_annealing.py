67af687ca5cdf01516b2a452b698f61dcdd328d5
SyrineBR <syrine.benrayana1925@gmail.com>
2022-12-28 22:35:26 +0100

    Correction of the bug of exceeding the maximum weight - Simulated annealing

--- Approximation_methods/Single_solution_algorithms/simulated_annealing.py
+++ Approximation_methods/Single_solution_algorithms/simulated_annealing.py
@@ -33,21 +33,20 @@ def select_initial_solution_randomly(numberOfElements, listOfElements, backPackS
     return [value, items_selection_string]
 
 #Test 
-#result = random_selection_initial_solution(4, [[9,6],[11,5],[13,9],[15,7]], 20)
+#result = select_initial_solution_randomly(4, [[9,6],[11,5],[13,9],[15,7]], 20)
 #print(result)
 
 # function that create a new solution based on the initial one (perturbation)
 def create_new_solution(initial_solution, listOfElements, backPackSize ):
     new_solution = list(initial_solution[1].strip())
     newValue = initial_solution[0]
-
     total_size = 0
-    for i in range(len(initial_solution)-1):
-        if initial_solution[i] == '1':
+    
+    for i in range(len(listOfElements)):
+        if initial_solution[1][i] == '1':
             total_size = total_size + listOfElements[i][1]
-
     while True:
-        index = random.randint(0,len(initial_solution)-1)
+        index = random.randint(0,len(listOfElements)-1)
 
         if new_solution[index] == '0':
             if total_size + listOfElements[index][1] > backPackSize:
@@ -63,7 +62,7 @@ def create_new_solution(initial_solution, listOfElements, backPackSize ):
     return [newValue, ''.join(e for e in new_solution)]
 
 #Test 
-#print(create_new_solution([33,'1110'], [[9,6],[11,5],[13,9],[15,7]], 20))
+#print(create_new_solution([161, '0111101001'], [[55, 95], [10, 4], [47, 60], [5, 32], [4, 23], [50, 72], [8, 80], [61, 62], [85, 65], [87, 46]], 269))
 
     
 def simulated_annealing_method(numberOfElements, listOfElements , backPackSize):
@@ -75,14 +74,14 @@ def simulated_annealing_method(numberOfElements, listOfElements , backPackSize):
     i = 0
 
     solution = select_initial_solution_randomly(numberOfElements, listOfElements, backPackSize)
-    print("Initial solution: ", solution)
+    #print("Initial solution: ", solution)
 
     while T>Tmin and i<nb_it:
         i = i + 1
 
         #Perturbation
         new_solution = create_new_solution(solution, listOfElements, backPackSize )
-        print("iteration ", i, " : new solution = ", new_solution)
+        #print("iteration ", i, " : new solution = ", new_solution)
 
         #Comparison
         diff = solution[0] - new_solution[0]
@@ -94,5 +93,5 @@ def simulated_annealing_method(numberOfElements, listOfElements , backPackSize):
                 solution = new_solution
         
         T = T * K
-    print("Final solution: ", solution)
+    #print("Final solution: ", solution)
     return solution
