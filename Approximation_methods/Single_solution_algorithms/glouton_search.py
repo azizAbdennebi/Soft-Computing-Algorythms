3810644851ec38c4a911db43ff6a5759316a1a67
zizouabd <zizouabd7@gmail.com>
2023-01-15 12:25:08 +0100

    glouton search optimised

--- Approximation_methods/Single_solution_algorithms/glouton_search.py
+++ Approximation_methods/Single_solution_algorithms/glouton_search.py
@@ -1,30 +1,26 @@
 def glouton_search_method(numberOfElements, listOfElements , backPackSize):
-    #variable initialization
-    position=-1
-    weight=0
-    optimalValue=0
-    objectsSelected=[0]*numberOfElements
-    listSelected=[[0,0]]*numberOfElements
+    
+
+
+    items = sorted(listOfElements, key=lambda x: x[0]/x[1], reverse=True)
+    total_value = 0
+    total_weight = 0
     result=list()
-    #sorting the elements of the backpack by decreasing value
-    listOfElementsSorted= list(reversed(sorted(listOfElements)))
-    #print(listOfElementsSorted)
-    #calculation of the optimal value and creation of the list of selected objects
-    for i in range(numberOfElements):
-        if weight+listOfElementsSorted[i][1]<=backPackSize:
-            #objectsSelected[i]=1
-            listSelected[i]=listOfElementsSorted[i]
-            if listSelected[i] in listOfElements:
-                    position=listOfElements.index(listSelected[i],0,len(listOfElements))
-                    objectsSelected[position]=1
-            optimalValue=optimalValue+listOfElementsSorted[i][0]
-            weight=weight+listOfElementsSorted[i][1]
-    #print(listSelected)
-    result.append(optimalValue)
-    result.append("".join(str(i) for i in objectsSelected))
-    return result
+    taken = [0] * len(items)
+    for i in range(len(items)):
+        if total_weight + items[i][1] <= backPackSize:
+            taken[listOfElements.index(items[i])] = 1
+            total_weight += items[i][1]
+            total_value += items[i][0]
+    result.append(total_value)
+    result.append("".join(str(i) for i in taken))
+    return(result)
+    print (total_value, total_weight, taken)
 
+# items = [(60, 20), (100, 50), (120, 30)]
+# max_weight = 50
 
+# print (glouton_search_method(2,items, max_weight))
 #Test
 #glouton_search_method(4,[[8,5],[11,7],[6,4],[4,3]],14)
 #glouton_search_method(3,[[3,4],[4,1],[6,5]],8)
